0b450789f0f627d613eca3aca2f81eeeaa4e9daf
Ivan Sabo <123sabo@gmail.com>
2022-05-17 13:50:26 +0200

    Add postgres db support and migration

--- go.mod
+++ go.mod
@@ -3,6 +3,12 @@ module github.com/ivan-sabo/garagesale
 go 1.18
 
 require (
+	github.com/GuiaBolso/darwin v0.0.0-20191218124601-fd6d2aa3d244
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/lib/pq v1.10.5
+)
+
+require (
+	github.com/DATA-DOG/go-sqlmock v1.5.0 // indirect
+	github.com/cznic/ql v1.2.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"flag"
 	"log"
 	"net/http"
 	"net/url"
@@ -11,6 +12,7 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/ivan-sabo/garagesale/schema"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
@@ -26,9 +28,21 @@ func main() {
 	}
 	defer db.Close()
 
+	flag.Parse()
+	switch flag.Arg(0) {
+	case "migrate":
+		if err := schema.Migrate(db); err != nil {
+			log.Fatal("applying migrations: ", err)
+		}
+		log.Println("Migrations complete")
+		return
+	}
+
+	ps := ProductService{db: db}
+
 	api := http.Server{
 		Addr:         "localhost:8000",
-		Handler:      http.HandlerFunc(ListProducts),
+		Handler:      http.HandlerFunc(ps.List),
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 5 * time.Second,
 	}
@@ -69,7 +83,7 @@ func main() {
 
 func openDB() (*sqlx.DB, error) {
 	q := url.Values{}
-	q.Set("sslmofe", "disabled")
+	q.Set("sslmode", "disable")
 	q.Set("timezone", "utc")
 
 	u := url.URL{
@@ -85,16 +99,29 @@ func openDB() (*sqlx.DB, error) {
 
 // Product is something we sell
 type Product struct {
-	Name     string `json:"name"`
-	Cost     int    `json:"cost"`
-	Quantity int    `json:"quantity"`
+	ID          string    `db:"product_id" json:"id"`
+	Name        string    `db:"name" json:"name"`
+	Cost        int       `db:"cost" json:"cost"`
+	Quantity    int       `db:"quantity" json:"quantity"`
+	DateCreated time.Time `db:"date_created" json:"date_created"`
+	DateUpdated time.Time `db:"date_updated" json:"date_updated"`
+}
+
+// ProductService has handler methods for dealing with Products
+type ProductService struct {
+	db *sqlx.DB
 }
 
 // ListProducts tells you about request you made
-func ListProducts(w http.ResponseWriter, r *http.Request) {
-	list := []Product{
-		{Name: "Comic books", Cost: 75, Quantity: 50},
-		{Name: "McDonalds toys", Cost: 25, Quantity: 125},
+func (p *ProductService) List(w http.ResponseWriter, r *http.Request) {
+	list := []Product{}
+
+	const q = `SELECT * FROM products`
+
+	if err := p.db.Select(&list, q); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("error querying db", err)
+		return
 	}
 
 	data, err := json.Marshal(list)
